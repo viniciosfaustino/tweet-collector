427a56049f6ee81df3555ca2e41670ca6b11ebbb
viniciosfaustino <vinicioskf@gmail.com>
2020-01-30 16:59:42 -0400

    implementado: salvar json com informações do tweet; coletar texto do tweet baseado no id, coletar todos os textos de todos os tweets salvos em um json; corrigido bug na obtenção do texto completo do tweet;

--- crawler.py
+++ crawler.py
@@ -4,7 +4,7 @@ from time import ctime
 import json
 import tweepy
 from dotenv import load_dotenv
-from tweetListener import TweetListener
+from tweetListener import TweetListener, get_text_from_status
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -21,17 +21,15 @@ api = tweepy.API(auth)
 
 
 class TweetCollector:
-    def __init__(self, name: str, save_tweets: bool = False, path: str = '.', max_tweets: int = 2,
-                 timeout: int = 50000, track: list = None):
+    def __init__(self, name: str, path: str = 'data', max_tweets: int = 10, timeout: int = 36000, track: list = None):
         self.name = name
         self.tweet_count = 0
-        self.save_tweets = save_tweets
         self.path = path
         self.listener = TweetListener(timeout, max_tweets)
         self.stream = tweepy.Stream(auth=api.auth, listener=self.listener)
         self.track = track
 
-    def save_tweets_to_file(self):
+    def save_tweets_to_file(self, id_only: bool = False):
         if os.path.exists(self.path):
             path = os.path.join(self.path, self.name)
             try:
@@ -40,30 +38,66 @@ class TweetCollector:
                 pass
             created_at = ctime()
             created_at = created_at.replace(' ', '-')
-            data = {'name': self.name, "id_str": self.listener.tweets, 'created_at': created_at}
+            if id_only:
+                data = {'name': self.name, 'created_at': created_at, "id_str": self.listener.tweets['id_str']}
+            else:
+                data = {'name': self.name, 'created_at': created_at, "id_str": self.listener.tweets['id_str'],
+                        'text': self.listener.tweets['text']}
             filename = self.name+"-"+str(created_at)+'.json'
             file = os.path.join(path, filename)
-            print(file)
+
             with open(file, "w") as f:
-                print(dir(f))
                 json.dump(data, f, indent=4)
-            print(data)
 
     def start_stream(self, **kwargs):
-        print('start stream')
         try:
-            print(self.track)
-            self.stream.filter(track=self.track)
+            self.stream.filter(track=self.track, )
         except Exception:
             self.stream.disconnect()
             self.start_stream(**kwargs)
 
-        self.tweet_count = len(self.listener.tweets)
-        print(self.tweet_count)
+        self.tweet_count = len(self.listener.tweets['id_str'])
 
+        print(self.tweet_count, "tweets saved!")
         self.save_tweets_to_file()
-        # aqui vai ser chamada a funcao que escreve no arquivo
 
 
-collector = TweetCollector("coletor", track=['odeio'])
-collector.start_stream()
+def get_text_from_tweet_id(id_str):
+    try:
+        status = api.get_status(id_str, tweet_mode="extended")
+        text = get_text_from_status(status)
+        text = text.replace("\n", " ")
+        return text
+
+    except:
+        raise Exception
+
+
+def retrieve_text_from_json(filepath: str):
+    with open(filepath, 'r') as f:
+        file = json.load(f)
+        ids = file['id_str']
+        text = []
+        total = len(ids)
+        cont = 1
+        for id_str in ids:
+            try:
+                tweet_str = get_text_from_tweet_id(id_str)
+                text.append(tweet_str)
+                print(cont, "of", total, "tweets retrieved")
+                cont += 1
+
+            except Exception:
+                pass
+
+    txt_filename = os.path.splitext(filepath)[0]+".txt"
+    with open(txt_filename, "w") as f:
+        for line in text:
+            f.write(line+"\n")
+
+
+# collector = TweetCollector("coronavirus", track=['coronavirus', 'corona virus', 'coronavírus', 'corona vírus', 'Wuhan',
+#                                                  '#nCoV2019', 'ncov2019', '2019ncov'])
+# collector.start_stream()
+
+retrieve_text_from_json("coronavirus/coronavirus-Thu-Jan-30-15:23:39-2020.json")
--- tweetListener.py
+++ tweetListener.py
@@ -5,28 +5,36 @@ import tweepy
 class TweetListener(tweepy.StreamListener):
     def __init__(self, timeout: int, max_tweets: int):
         super(TweetListener, self).__init__()
-        self.started_at = time()
         self.max_tweets = max_tweets
+        self.started_at = time()
         self.timeout = timeout
-        self.tweets = []
+        self.tweets = {"id_str": [], 'text': []}
 
     def on_status(self, status):
-        if time() - self.started_at < self.timeout and len(self.tweets) < self.max_tweets:
+        if time() - self.started_at < self.timeout and len(self.tweets['id_str']) < self.max_tweets:
             # print_status_text(status)
-            self.tweets.append(status.id_str)
+            text = get_text_from_status(status)
+            self.tweets['id_str'].append(status.id_str)
+            self.tweets['text'].append(text)
         else:
             return False
 
 
-def print_status_text(status):
+def get_text_from_status(status):
     if hasattr(status, 'retweeted_status'):
         try:
             text = status.retweeted_status.extended_tweet["full_text"]
         except AttributeError:
-            text = status.retweeted_status.text
+            text = status.retweeted_status.full_text
     else:
         try:
             text = status.extended_tweet["full_text"]
         except AttributeError:
-            text = status.text
-    print(text)
+            text = status.full_text
+
+    return text
+
+
+def print_status_text(status):
+    text = get_text_from_status(status)
+    print(text[:10], '\n')
